eec2bd415f36bc955e6c2a3d1cc5d5627534dedc
Mauro <116788542+mauromltn@users.noreply.github.com>
2024-09-25 19:29:47 +0000

    feat: Improve header styling and add animation to hero section
    - Update the language attribute to Italian
    - Improved the styling of the header section.
    - Add a hero section with an animated caption and background image.
    - The trips section is added with a background color.

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,10 +1,18 @@
 window.addEventListener('scroll', function() {
-    const scrollPosition = window.scrollY; // Ottieni la posizione di scroll
-    const textElements = document.querySelectorAll('.text');
+  const scrollPosition = window.scrollY; // Ottieni la posizione di scroll
+  const textElements = document.querySelectorAll('.text');
+  const islandElement = document.querySelector('.caption');
 
-    // Calcola lo spostamento del testo in base alla posizione di scroll
-    textElements.forEach((text) => {
-      const offset = scrollPosition * 0.5; // Modifica il fattore per cambiare l'effetto
-      text.style.transform = `translateY(-${offset}px)`; // Muovi il testo verso l'alto
-    });
+  // Calcola lo spostamento del testo in base alla posizione di scroll
+  textElements.forEach((text) => {
+    const offset = scrollPosition * 1.5; // Modifica il fattore per cambiare l'effetto
+    text.style.transform = `translateY(-${offset}px)`; // Muovi il testo verso l'alto
   });
+
+  // Sposta l'elemento .island sotto
+  const isolaOffset = scrollPosition * 1.5; // Modifica il fattore per cambiare l'effetto
+  islandElement.style.transform = `translateY(+${isolaOffset}px)`; // Muovi l'elemento verso il basso
+  // Calcola la scala in base alla posizione di scroll
+  const scale = 1 - (scrollPosition * 0.001); // Modifica il fattore per cambiare l'effetto
+  islandElement.style.transform += ` scale(${scale})`; // Riduci l'elemento gradualmente
+});
