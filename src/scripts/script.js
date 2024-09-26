f6284cdce3e0dd38128fbecbc8576205fe9ef639
Mauro <116788542+mauromltn@users.noreply.github.com>
2024-09-26 15:19:25 +0000

    feat: update the .hero animation

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -3,24 +3,22 @@ window.addEventListener('scroll', function() {
   const textElements = document.querySelectorAll('.text');
   const islandElement = document.querySelector('.caption');
 
-  // Calcola lo spostamento del testo in base alla posizione di scroll
   textElements.forEach((text) => {
-    const textOffset = scrollPosition * 1; // Modifica il fattore per cambiare l'effetto
+    const textOffset = scrollPosition * 2.5;
     text.style.transform = `translateY(-${textOffset}px)`; // Muovi il testo verso l'alto
   });
 
-  // Limita lo spostamento dell'elemento .caption
-  const maxScroll = 800; // Imposta il limite massimo di scroll
-  const islandOffset = Math.min(scrollPosition * 1.5, maxScroll); // Limita lo spostamento massimo
-  islandElement.style.transform = `translateY(${islandOffset}px)`; // Muovi l'elemento verso il basso senza superare il limite
-
-  // Calcola la scala in base alla posizione di scroll
-  const scale = 1 - (scrollPosition * 0.001); // Modifica il fattore per cambiare l'effetto
-  islandElement.style.transform += ` scale(${scale})`; // Riduci l'elemento gradualmente
+  let islandOffset = scrollPosition * 1.5;
+  const maxIslandOffset = 700; // Imposta un limite massimo per l'offset
+  if (islandOffset > maxIslandOffset) {
+    islandOffset = maxIslandOffset;
+  }
+  const scale = Math.max(1 - (scrollPosition * 0.001), 0); // Modifica il fattore per cambiare l'effetto e imposta un limite minimo
+  islandElement.style.transform = `translateY(${islandOffset}px) scale(${scale})`;
 });
 
 const cursor = document.querySelector('.cursor');
 document.addEventListener('mousemove', (e) => {
   cursor.style.left = e.clientX + 'px';
   cursor.style.top = e.clientY + 'px';
-  })
+});
