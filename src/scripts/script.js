ccb0312c0f3e01cea5c9364de2a6057b0351cbb2
Mauro <116788542+mauromltn@users.noreply.github.com>
2024-09-26 14:14:13 +0000

    feat: Add cursor animation and fix image scroll
    - Add a cursor animation with a white dot shape.
    - Fix the infinite scroll of the .caption image

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -5,14 +5,22 @@ window.addEventListener('scroll', function() {
 
   // Calcola lo spostamento del testo in base alla posizione di scroll
   textElements.forEach((text) => {
-    const offset = scrollPosition * 1.5; // Modifica il fattore per cambiare l'effetto
-    text.style.transform = `translateY(-${offset}px)`; // Muovi il testo verso l'alto
+    const textOffset = scrollPosition * 1; // Modifica il fattore per cambiare l'effetto
+    text.style.transform = `translateY(-${textOffset}px)`; // Muovi il testo verso l'alto
   });
 
-  // Sposta l'elemento .island sotto
-  const isolaOffset = scrollPosition * 1.5; // Modifica il fattore per cambiare l'effetto
-  islandElement.style.transform = `translateY(+${isolaOffset}px)`; // Muovi l'elemento verso il basso
+  // Limita lo spostamento dell'elemento .caption
+  const maxScroll = 800; // Imposta il limite massimo di scroll
+  const islandOffset = Math.min(scrollPosition * 1.5, maxScroll); // Limita lo spostamento massimo
+  islandElement.style.transform = `translateY(${islandOffset}px)`; // Muovi l'elemento verso il basso senza superare il limite
+
   // Calcola la scala in base alla posizione di scroll
   const scale = 1 - (scrollPosition * 0.001); // Modifica il fattore per cambiare l'effetto
   islandElement.style.transform += ` scale(${scale})`; // Riduci l'elemento gradualmente
 });
+
+const cursor = document.querySelector('.cursor');
+document.addEventListener('mousemove', (e) => {
+  cursor.style.left = e.clientX + 'px';
+  cursor.style.top = e.clientY + 'px';
+  })
